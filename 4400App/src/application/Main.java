51f71d9a1b3aaf41e9a367860968bf2887f98095
pkalaga1 <prava.kalaga@gmail.com>
2017-07-19 11:26:04 +0200

    This is the attraction information page

--- 4400App/src/application/Main.java
+++ 4400App/src/application/Main.java
@@ -26,7 +26,7 @@ public class Main extends Application {
 			
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(Main.class.
-					getResource("../view/Login.fxml"));
+					getResource("../view/AttractionPage.fxml"));
 			
 			Parent root = loader.load();
 			
