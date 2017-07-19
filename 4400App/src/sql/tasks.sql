8d1a6e5492f4507351f83dcb9b4d354c7bed15ea
raghavrajmittal <raj.raghav@gmail.com>
2017-07-19 10:51:52 +0200

    created task file

---
+++ 4400App/src/sql/tasks.sql
@@ -0,0 +1,12 @@
+-- CS 4400 SQL Queries for Tasks:
+
+-- 1.0 Login (Raghav)
+SELECT Email, IsSuspended, IsManager
+FROM USER
+WHERE Email = " " AND Password = " "
+
+-- 2.0 Registration (Raghav)
+SELECT Email 
+FROM USER
+WHERE Email = " "
+
