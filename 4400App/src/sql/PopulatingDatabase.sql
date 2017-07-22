463d8d1a2da0ae290c72b4432ec0e85ea47838b5
raghavrajmittal <raj.raghav@gmail.com>
2017-07-22 15:09:30 +0200

    tasks done and tested the Populating Db file

---
+++ 4400App/src/sql/PopulatingDatabase.sql
@@ -0,0 +1,209 @@
+-- Populating the DB
+
+INSERT INTO User
+VALUES ("maya@gmail.com", 1, now(), FALSE, TRUE);
+
+INSERT INTO User
+VALUES ("raghav@gmail.com", 2, now(), FALSE, TRUE);
+
+INSERT INTO user
+VALUES ("pravan@gmail.com", 3, now(), FALSE, TRUE);
+ 
+INSERT INTO user
+VALUES ("alyssa@gmail.com", 4, now(), FALSE, TRUE);
+ 
+INSERT INTO user
+VALUES ("kendall@gmail.com", 5, now(), FALSE, FALSE);
+ 
+INSERT INTO user
+VALUES ("annalise@gmail.com",6 , now(), TRUE, FALSE);
+ 
+INSERT INTO user
+VALUES ("bent@gmail.com", 7, now(), FALSE, FALSE);
+ 
+INSERT INTO user
+VALUES ("adya@gmail.com", 8, now(), TRUE, FALSE);
+
+
+INSERT INTO user
+VALUES ("mattl@gmail.com", 9, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("sarahg@gmail.com", 10, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("joseph@gmail.com", 11, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("walker@gmail.com", 12, now(), FALSE, TRUE);
+INSERT INTO user
+VALUES ("anthony@gmail.com", 13, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("chloe@gmail.com", 14, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("eugenia@gmail.com", 15, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("chris@gmail.com", 16, now(), TRUE, FALSE);
+
+INSERT INTO user
+VALUES ("thy@gmail.com", 17, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("xing@gmail.com", 18, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("abbie@gmail.com", 19, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("elizabeth@gmail.com", 20, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("kyra@gmail.com", 21, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("ruchi@gmail.com", 22, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("michael@gmail.com", 22, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("savy@gmail.com", 23, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("stephan@gmail.com", 24, now(), FALSE, FALSE);
+
+INSERT INTO user
+VALUES ("charleston@gmail.com", 25, now(), FALSE, FALSE);
+
+
+INSERT INTO REVIEWABLE_ENTITY
+VALUES ("thy@gmail.com", 23, now(), TRUE), ("xing@gmail.com", 25, now(), TRUE);  -- PENDING CITIES
+
+INSERT INTO REVIEWABLE_ENTITY
+VALUES ("michael@gmail.com", 13, now(), TRUE), ("abbie@gmail.com", 7, now(), TRUE); -- pending attractions
+
+INSERT INTO REVIEWABLE_ENTITY
+VALUES ("kendall@gmail.com", 20, now(), FALSE), ("bent@gmail.com", 21, now(), FALSE), ("mattl@gmail.com", 22, now(), FALSE), ("sarahg@gmail.com", 24, now(), FALSE), ("joseph@gmail.com", 26, now(), FALSE); -- cities
+
+INSERT INTO REVIEWABLE_ENTITY
+VALUES ("walker@gmail.com", 1, now(), FALSE), ("anthony@gmail.com", 2, now(), FALSE), ("chloe@gmail.com", 3, now(), FALSE), ("eugenia@gmail.com", 4, now(), FALSE), ("elizabeth@gmail.com", 5, now(), FALSE), ("kyra@gmail.com", 6, now(), FALSE), ("ruchi@gmail.com", 9, now(), FALSE), ("savy@gmail.com", 8, now(), FALSE), ("Stephan@gmail.com", 10, now(), FALSE), ("Charleston@gmail.com", 11, now(), FALSE), ("maya@gmail.com", 12, now(), FALSE), ("pravan@gmail.com", 14, now(), FALSE), ("alyssa@gmail.com", 15, now(), FALSE); -- attractions 
+
+INSERT INTO REVIEW
+VALUES ("kendall@gmail.com", 20, now(),"Amazing beaches. Very crowded", 4), ("bent@gmail.com", 21, now(),"lots of good food! Very expensive", 4), ("mattl@gmail.com", 22, now(),"Lots to see. Love the accent!", 4), ("sarahg@gmail.com", 24, now(),"Not a fan of the nightlife. Not much to do", 1), ("joseph@gmail.com", 26, now(),"Very good culture; very hipster", 4); -- cities
+
+INSERT INTO REVIEW
+VALUES ("walker@gmail.com", 1, now(), "Beautiful Church. Not worth the price", 3), ("anthony@gmail.com", 2, now(), "Great spot to take photos—lots of tourists", 4), ("chloe@gmail.com", 3, now(), "I SAW NEYMAR! LOVE IT",4), ("eugenia@gmail.com", 4, now(), "Stunning but very crowded", 4), ("elizabeth@gmail.com", 5, now(),  "Beautiful. Felt very spiritual", 4), ("kyra@gmail.com", 6, now(), "Beautful paintings. Saw the Mona Lisa!", 4), ("ruchi@gmail.com", 9, now(), "Great views", 3), ("savy@gmail.com", 8, now(), "Not worth the price", 1), ("Stephan@gmail.com", 10, now(), "too many paintings", 1), ("Charleston@gmail.com", 11, now(), "Had lots of fun", 4), ("maya@gmail.com", 12, now(), "Love learning about Netherlands!", 4), ("pravan@gmail.com", 14, now(), "Great place to walk around but very pricey", 3), ("alyssa@gmail.com", 15, now(), "beautiful but very windy", 4); -- attractions 
+
+
+
+INSERT INTO CITY
+VALUES (20, "Barcelona", "Spain", NULL);
+
+INSERT INTO City
+VALUES (21, "Paris", "France", NULL);
+
+INSERT INTO City
+VALUES (22, "London", "England", NULL);
+
+INSERT INTO City
+VALUES (23, "Dublin", "Ireland", NULL); -- pending
+
+INSERT INTO City
+VALUES (24, "Amsterdam", "Nethlands", NULL);
+
+INSERT INTO City
+VALUES (25, "Bucharest", "Romania", NULL); -- pending
+
+INSERT INTO City
+VALUES (26, "San Francisco", "United States", "California");
+
+
+
+
+
+INSERT INTO ATTRACTION
+VALUES (1,20, "Sagrada Familia", "Carrer de Mallorca, 401, 08013 Barcelona, Spain", "Beautiful Church. Gaudi’s Masterpiece");
+
+INSERT INTO ATTRACTION
+VALUES (2,20, "Parc Guell", "08024 Barcelona", "Public park system composed of gardens and architectonic elements");
+
+INSERT INTO ATTRACTION
+VALUES (3,20, "Camp Nou", "Carrer d'Aristides Maillol, 12, 08028 Barcelona, Spain", "Home of Barcelona’s premier football team");
+
+INSERT INTO ATTRACTION
+VALUES (4,21, "Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", "Wrought iron lattice tower on Champ de Mars");
+
+INSERT INTO ATTRACTION
+VALUES (5,21, "Notre Dame", "6 Paris Notre-Dame - Pl. Jean-Paul II, 75004 Paris, France
+", "Medieval Catholic cathedral");
+
+INSERT INTO ATTRACTION
+VALUES (6,21, "Louvre Museum", "Rue de Rivoli, 75001 Paris, France", "Worlds largest museum");
+
+INSERT INTO ATTRACTION
+VALUES (7,22, "Big Ben", "Westminster, London SW1A 0AA, UK", "Great Bell of the clock of Palace of Westminister");
+
+INSERT INTO ATTRACTION
+VALUES (8,22, "London Eye", " Lambeth, London SE1 7PB, UK", "Giant Ferris wheel on the South Bank of the River Thames");
+
+INSERT INTO ATTRACTION
+VALUES (9,22, "Tower of London", "St Katharine's & Wapping, London EC3N 4AB, UK
+", "Officially Her Majesty’s Royal Palace and Fortress of the Tower of London");
+
+INSERT INTO ATTRACTION
+VALUES (10,23, "Van Gough Museum", "Museumplein 6, 1071 DJ Amsterdam, Netherlands", "Art museum dedicated to the works of Vincent Van Gough");
+
+INSERT INTO ATTRACTION
+VALUES (11,23, "Efteling", " Europalaan 1, 5171 KW Kaatsheuvel, Netherlands
+", "Fantasy-themed amusement park based on elements of ancient myths");
+
+INSERT INTO ATTRACTION
+VALUES (12,23, "Rijksmuseum", " Museumstraat 1, 1071 XX Amsterdam, Netherlands", "Dutch cultural and history museum");
+
+INSERT INTO ATTRACTION
+VALUES (13,24, "Golden Gate Bridge", " Golden Gate Bridge, San Francisco, CA, USA
+", "Suspension bridge spanning the Golden Gate strait");
+
+INSERT INTO ATTRACTION
+VALUES (14,24, "Union Square", "Union Square, San Francisco, CA, USA
+", "2.6 public plaza in the center of San Francisco");
+
+INSERT INTO ATTRACTION
+VALUES (15,24, "Fishermans Warf", "Fishermans Warf, San Francisco, CA, USA
+", "Neighborhood and popular tourist attraction that borders the coast");
+
+
+
+
+
+
+
+INSERT INTO CONTACT_INFO
+VALUES (1, "Phone Number", "3815647890"), (2, "Email", "gaudi@gmail.com"),
+(3, "Email", "Neymar@gmail.com"), (4, "Phone Number", "56789345670"), 
+(5, "Email", "hunchback@gmail.com"), (6, "Email", "monalisa@gmail.com"), 
+(7, NULL, NULL), (8, "Phone Number", "8975431212"), (9, "Phone Number", "7684325567"), 
+(10, "Email", "starrynight@gmail.com"), (11, "Phone Number", "8976543321"), (12, "Phone Number", "9897765656"), 
+(13, "Phone Number", "7890009090"), (14, "Email", "shopping@gmail.com");
+
+INSERT INTO HOURS_OF_OPERATION
+VALUES (1,"Mon-Sun","090000","150000"), (2,"Mon-Sun", 100000,"210000"), (3,"Mon-Sun","080000","173000"), 
+(4,"Monday","090000","200000"), (5,"Mon-Sun","090000","190000"), (6,"Mon-Sun","100000","180000"), 
+(7,"Mon-Sun", "090000","180000"), (8,"Mon-Sun","110000","180000"), (9,"Mon-Sun","100000","150000"), 
+(10,"Mon-Sun","090000","180000"), (11,"Saturday","103000","210000"), (12,"Mon-Sun","090000","200000"), 
+(13,"Mon-Sun","000000","000000"), (14,"Mon-Sun","000000","000000"), (15,"Weekends","000000","000000");
+
+
+INSERT INTO CATEGORY
+VALUES ("Museum"),("Religious"),("Parks/Public Space"),("Amusement Park"),("Shopping"),("Food"),("Nightlife"),("Adventure"),("Monument"),("Sports");
+
+
+
+
+
+
+INSERT INTO FALLS_UNDER
+VALUES (1, "Religious"), (2, "Parks/Public Space"), (3, "Sports"), (4, "Monument"), (5, "Religious"), (6, "Museum"), (7, "Monument"), (8, "Monument"), (9, "Monument"), (10, "Museum"), (11, "Amusement Park"), (12, "Museum"), (13, "Monument"), (14, "Parks/Public Space"), (15, "Food");
--- 4400App/src/sql/tasks.sql
+++ 4400App/src/sql/tasks.sql
@@ -1,66 +1,337 @@
+
 -- CS 4400 SQL Queries for Tasks:
+-- Contact Raghav Mittal for any queries (pun intended) :P
+
 
 -- 1.0 Login (Raghav)
 SELECT Email, IsSuspended, IsManager
 FROM USER
-WHERE Email = " " AND Password = " ";
-
+WHERE Email = "maya2@gmail.com" AND Password = 2;
 
 -- 2.0 Registration (Raghav)
 SELECT Email 
 FROM USER
-WHERE Email = " ";
+WHERE Email = "raghav@gmail.com";
 
 INSERT INTO USER
-VALUES ("email", "Password", now(), FALSE, FALSE)
+VALUES ("raghav2@gmail.com", 2, now(), FALSE, FALSE);
 
 
 -- 3.0 Regular User Welcome (Raghav)
-SELECT DISTINCT NAME
-FROM CITY -- drop down for city
+
+ 
+SELECT DISTINCT Name
+FROM CITY as C, REVIEWABLE_ENTITY as E-- drop down for city
+WHERE C.CityID = E.EntityID AND E.IsPending = FALSE;
 
 SELECT *
-FROM CATEGORY -- drop down for category
+FROM CATEGORY ;-- drop down for category
 
 DELETE FROM USER
-WHERE Email = "" -- delete account
+WHERE Email = ""; -- delete accountCITY
 
 
 -- 4.0 All Cities List (Raghav)
-(
-	SELECT C.Name, C.Country, C.State, AVG(Rating), COUNT(Rating)
-	FROM CITY AS C, REVIEW AS R
-	WHERE C.CityID = R.EntityID 
-	GROUP BY DISTINCT C.CityID
-) inner join (
-	SELECT C.Name, COUNT(*)
-	FROM CITY AS T, ATTRACTION AS A
-	WHERE T.CityID = A.LocatedIn
-	GROUP BY DISTINCT T.NAME
-) on C.Name = T.Name; -- TEST IT OUT (Also need to add a check to not display isPending cities)
+SELECT res1.Name + Country + State AS City, res1.avgRat, res1.totalRat, res2.numAttr
+FROM (
+
+
+	(SELECT C.Name, C.Country, C.State, AVG(Rating) as avgRat, COUNT(Rating) as totalRat
+	FROM CITY AS C, REVIEW AS R, REVIEWABLE_ENTITY as E
+	WHERE C.CityID = R.EntityID and C.CityID = E.EntityID and E.IsPending = FALSE
+	GROUP BY C.CityID ) as res1
+    
+    inner join 
+	
+	(SELECT T.Name, COUNT(*) as numAttr
+	FROM CITY AS T, ATTRACTION AS A, REVIEWABLE_ENTITY as E
+	WHERE T.CityID = A.LocatedIn and A.AttrID = E.EntityID and E.IsPending = FALSE
+	GROUP BY T.NAME) as res2
+    
+    on res1.name = res2.name
+    ) ;
+    -- also order it by something?
 
 
 -- 4.1 New City Form (R)
 INSERT INTO REVIEWABLE_ENTITY
-VALUES ("username_field", EntityID, now(), TRUE)
+VALUES ("username_field", EntityID, now(), !IsManager);
 INSERT INTO CITY
-VALUES (EntityID, "name_field", 'country_field', 'state_field')
+VALUES (EntityID, "name_field", 'country_field', 'state_field');
 -- !TODO Check if city already exists
 
 
 -- 5.0 Sample City Page (R)
-(
-	SELECT AttrID, LocatedIn, Name, Address, Category
-	FROM ATTRACTION, FALLS_UNDER
-	WHERE LocatedIn = "CityID_field" AND ATTRACTION.AttrID = FALLS_UNDER.AttrID
-) inner join (	
-	SELECT A.AttrID, AVG(Rating), COUNT(Rating)
+SELECT res1.AttrID, Name, Address, Category, avgRat, numRat
+FROM(
+
+	(SELECT A.AttrID, LocatedIn, Name, Address
+	FROM ATTRACTION as A, REVIEWABLE_ENTITY as E
+	WHERE LocatedIn = "CityID_field" and A.AttrID = E.EntityID and E.IsPending = FALSE) as res1
+inner join 
+	(SELECT A.AttrID, AVG(Rating) as avgRat, COUNT(Rating) as numRat
 	FROM ATTRACTION AS A, REVIEW AS R
-	WHERE A.AttrID = R.EntityID AND LocatedIn = "CityID_field"
-	GROUP BY DISTINCT A.AttrID
-) on AttrID = A.AttrID;
---check this, seems different from All cities list (4.0)
+	WHERE A.AttrID = R.EntityID AND A.LocatedIn = "CityID_field"
+	GROUP BY A.AttrID) as res2
+on res1.AttrID = res2.AttrID 
+
+inner join 
+	(SELECT A.AttrID, GROUP_CONCAT(F.Category) as Category
+	FROM ATTRACTION AS A, FALLS_UNDER AS F, REVIEWABLE_ENTITY as E
+	WHERE A.AttrID = F.AttrID and A.AttrID = E.EntityID and E.IsPending = FALSE
+	GROUP BY A.AttrID) as res3
+on res1.AttrID = res3.AttrID);
+
+
+
+-- 5.1 City Review Form/ Update Form
+SELECT Comment, Rating
+FROM REVIEW
+WHERE Email = "email_field" AND EntityID = "entityID_field";
+
+UPDATE REVIEW
+SET Rating="rating_field" AND Comment="comment_field"
+WHERE Email = "email_field" AND EntityID = "entityID_field";
+
+INSERT INTO REVIEW
+VALUES ("email_field", "entityID_field", now(), "comment_field", "rating_field");
+
+DELETE FROM REVIEW
+WHERE Email = "email_field" AND EntityID = "entityID_field";
+
+
+
+-- 5.2 City's Review Page
+SELECT Email, Rating, Comment
+FROM REVIEW AS R
+WHERE "cityID_field" = R.EntityID
+
+
+
+-- 5.3 Sample City Page narrowed down by category
+SELECT res1.AttrID, Name, Address, Category, avgRat, numRat
+FROM(
+
+	(SELECT A.AttrID, LocatedIn, Name, Address
+	FROM ATTRACTION as A, REVIEWABLE_ENTITY as E
+	WHERE LocatedIn = "CityID_field" and A.AttrID = E.EntityID and E.IsPending = FALSE) as res1
+inner join 
+	(SELECT A.AttrID, AVG(Rating) as avgRat, COUNT(Rating) as numRat
+	FROM ATTRACTION AS A, REVIEW AS R
+	WHERE A.AttrID = R.EntityID AND A.LocatedIn = "CityID_field"
+	GROUP BY A.AttrID) as res2
+on res1.AttrID = res2.AttrID 
+
+inner join 
+	(SELECT A.AttrID, GROUP_CONCAT(F.Category) as Category
+	FROM ATTRACTION AS A, FALLS_UNDER AS F, REVIEWABLE_ENTITY as E
+	WHERE A.AttrID = F.AttrID and A.AttrID = E.EntityID and E.IsPending = FALSE
+	GROUP BY A.AttrID) as res3
+on res1.AttrID = res3.AttrID)
+
+inner join
+	(SELECT AttrID
+	FROM FALLS_UNDER
+	WHERE Category = "category_field") as res4
+ON res1.AttrID = res4.AttrID
+
+order by numRat desc;
+
+
+
+-- 6.0 User's Review Page
+(SELECT C.Name, R.Comment, R.Rating
+FROM REVIEW AS R, CITY AS C
+WHERE "Email_field" = R.Email AND C.CityID = R.EntityID)
+UNION
+(SELECT A.Name , R.Comment, R.Rating
+FROM REVIEW AS R, ATTRACTION AS A
+WHERE "Email_field" = R.Email AND A.AttrID = R.EntityID);
+-- do we display IsPending reviews?
 
 
 
 
+-- 7.0 Attraction's List
+SELECT res1.AttrID, res1.Name, Category, res1.LocatedIn, avgRat, numRat
+from
+
+(SELECT A.AttrID, A.Name, A.LocatedIn, Avg(Rating) as avgRat, COUNT(Rating) as numRat
+FROM ATTRACTION AS A, REVIEW AS R,  REVIEWABLE_ENTITY as E
+WHERE A.AttrID = R.EntityID and A.AttrID = E.EntityID and E.IsPending = FALSE
+GROUP BY R.EntityID) as res1
+ 
+inner join 
+
+(SELECT A.AttrID, GROUP_CONCAT(F.Category) as Category
+FROM ATTRACTION AS A, FALLS_UNDER AS F, REVIEWABLE_ENTITY as E
+WHERE A.AttrID = F.AttrID and A.AttrID = E.EntityID and E.IsPending = FALSE
+GROUP BY A.AttrID) as res2
+
+on res1.AttrID = res2.AttrID
+order by res1.LocatedIn asc;
+
+
+-- 7.1 New Attraction Form
+SELECT DISTINCT Name
+FROM CITY, REVIEWABLE_ENTITY-- drop down for city
+WHERE CITY.CityID = REVIEWABLE_ENTITY.EntityID AND REVIEWABLE_ENTITY.IsPending = FALSE;
+
+INSERT INTO REVIEWABLE_ENTITY
+VALUES ("email_field", "entityID_field", now(), !IsManager);
+INSERT INTO ATTRACTION
+VALUES ("entityID_field", "LocatedIn_field", "name_field", "Address_field", "description_field");
+INSERT INTO REVIEW
+VALUES ("Email_field", "entityID_field", now(), "comment_field", "rating_field");
+INSERT INTO FALLS_UNDER
+VALUES ("entityID_field", "category_field"); -- do this in a for loop in the java code
+INSERT INTO CONTACT_INFO
+VALUES ("entityID_field", "Contact_method_field", "method_value_field"); -- only if it exists?
+INSERT INTO HOURS_OF_OPERATION
+VALUES ("entityID_field", "day_of_the_week_field", "open_time_field", "close_time_field");
+
+-- 8.0 Sample Attraction Page
+SELECT LocatedIn, Name, Address, Description
+FROM ATTRACTION
+WHERE AttrID = "attrID_field";
+
+SELECT AVG(Rating) as avgRat, COUNT(Rating) as numRat
+FROM REVIEW
+WHERE EntityID = "attrID_field";
+
+SELECT GROUP_CONCAT(DayOfTheWeek + ": " + OpenTime + "-" + CloseTime + " ") as HoursOfoperation
+FROM HOURS_OF_OPERATION
+WHERE AttrID = "attrID_field"
+GROUP by AttrID;
+
+SELECT GROUP_CONCAT(ContactMethod + ":" +MethodValue + " ") as ContactInfo
+FROM CONTACT_INFO
+WHERE AttrID = "attrID_field";
+
+SELECT GROUP_CONCAT(Category) as Category
+FROM FALLS_UNDER
+WHERE AttrID = "attrID_field";
+
+
+-- 8.1 Attraction Review/update form
+SELECT Comment, Rating
+FROM REVIEW
+WHERE Email = "email_field" AND EntityID = "entityID_field";
+
+UPDATE REVIEW
+SET Rating="rating_field" AND Comment="comment_field"
+WHERE Email = "email_field" AND EntityID = "entityID_field";
+
+INSERT INTO REVIEW
+VALUES ("email_field", "entityID_field", now(), "comment_field", "rating_field");
+
+DELETE FROM REVIEW
+WHERE Email = "email_field" AND EntityID = "entityID_field";
+
+
+-- 8.2 Attraction's Review Page
+SELECT SubmittedBy, Rating, Comment
+FROM REVIEW
+WHERE EntityID = "attrID_field"
+ORDER BY Rating DESC;
+-- options to sort too
+
+
+--8.3 Manager's Welcome Page
+ 
+SELECT DISTINCT Name
+FROM CITY as C, REVIEWABLE_ENTITY as E-- drop down for city
+WHERE C.CityID = E.EntityID AND E.IsPending = FALSE;
+
+SELECT *
+FROM CATEGORY ;-- drop down for category
+
+DELETE FROM USER
+WHERE Email = ""; -- delete accountCITY
+
+--Category Page
+SELECT res2.Category, res2.numAttr
+FROM (
+
+(SELECT CName
+FROM CATEGORY) as res1
+
+inner join
+
+(SELECT F.Category, COUNT(F.AttrID) as numAttr
+FROM FALLS_UNDER AS F, REVIEWABLE_ENTITY AS E
+WHERE F.AttrID = E.EntityID and E.IsPending = FALSE
+GROUP BY F.Category) as res2
+
+on res1.CName = res2.Category)
+order by Category ASC;
+-- checks for whether deletign the category will delete an attraction
+
+
+-- 10.0/ 10.1 user's list
+SELECT Email, DateJoined, IsSuspended, IsManager
+FROM USER;  -- Grouping is needed
+
+UPDATE USER
+SET IsManager = TRUE
+WHERE Email = "email_field";
+
+UPDATE USER
+SET IsManager = FALSE
+WHERE Email = "email_field";
+
+UPDATE USER
+SET IsSuspended = TRUE
+WHERE Email = "email_field";
+
+UPDATE USER
+SET IsSuspended = FALSE
+WHERE Email = "email_field";
+
+DELETE FROM USER
+WHERE Email = "email_field";
+
+
+
+-- 11.0 Pending Cities List
+SELECT E.EntityID, C.Name, C.Country, E.SubmittedBy, R.Rating, R.Comment
+FROM CITY AS C, REVIEW AS R, REVIEWABLE_ENTITY as E
+WHERE C.CityID = R.EntityID and C.CityID = E.EntityID and E.IsPending = TRUE
+ORDER by C.Name ASC;
+
+UPDATE REVIEWABLE_ENTITY
+SET IsPending = FALSE
+WHERE EntityID = "entityID_field";
+
+DELETE FROM REVIEWABLE_ENTITY
+WHERE EntityID = "entityID_field";
+
+
+-- 12.0 Pending Attraction's Page
+SELECT res1.AttrID, Name, LocatedIn, Address, Category, Description, HoursOfOperation, ContactInfo, SubmittedBy, Rating, Comment
+from
+
+	(SELECT A.AttrID, A.Name, A.LocatedIn, A.Address, A.Description, E.SubmittedBy, R.Rating, R.Comment
+	FROM ATTRACTION AS A, REVIEW AS R,  REVIEWABLE_ENTITY as E
+	WHERE A.AttrID = R.EntityID and A.AttrID = E.EntityID and E.IsPending = TRUE) as res1
+inner join 
+	(SELECT A.AttrID, GROUP_CONCAT(F.Category) as Category
+	FROM ATTRACTION AS A, FALLS_UNDER AS F, REVIEWABLE_ENTITY as E
+	WHERE A.AttrID = F.AttrID and A.AttrID = E.EntityID and E.IsPending = FALSE
+	GROUP BY A.AttrID) as res2
+on res1.AttrID = res2.AttrID
+
+inner join
+	(SELECT AttrID, GROUP_CONCAT(DayOfTheWeek + ": " + OpenTime + "-" + CloseTime + " ") as HoursOfoperation
+	FROM HOURS_OF_OPERATION
+	WHERE AttrID = "attrID_field"
+	GROUP by AttrID) as res3
+on res1.AttrID = res3.AttrID
+
+inner join
+	(SELECT AttrID, GROUP_CONCAT(ContactMethod + ":" +MethodValue + " ") as ContactInfo
+	FROM CONTACT_INFO
+	WHERE AttrID = "attrID_field") as res4
+on res1.AttrID = res4.AttrID
+
+order by res1.LocatedIn asc;
